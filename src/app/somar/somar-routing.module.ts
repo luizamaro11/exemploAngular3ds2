83ce5c7a6187d9a854d156be4c8ff666002726a0
luizamaro11 <luiz.amaro90@gmail.com>
2019-10-21 14:21:29 -0300

    criando o arquivo somar-routing.module para gerenciar as rotas do modulo somar

---
+++ src/app/somar/somar-routing.module.ts
@@ -0,0 +1,11 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { SomarComponent } from './components/somar';
+
+export const RouterSomar = [
+    { 
+        path: 'somar',
+        component: SomarComponent 
+    }
+]
+
